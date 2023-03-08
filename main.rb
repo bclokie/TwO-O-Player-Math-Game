efde695e558ad16be0d9b41d4ea86b6572dda26f
Braden <braden.clokie@gmail.com>
2023-03-08 12:12:57 -0500

    Added main.rb

---
+++ main.rb
@@ -0,0 +1,19 @@
+player1 = Player.new(1)
+player2 = Player.new(2)
+game = Game.new(player1, player2)
+
+until game.over?
+  current_player = game.current_player
+  question = Question.new
+  answer = UI.prompt(current_player, question)
+  if answer == question.answer
+    UI.correct
+  else
+    UI.incorrect(current_player)
+    current_player.lose_life
+  end
+  UI.scoreboard(game)
+  game.next_turn
+end
+
+UI.announce_winner(game.winner)
